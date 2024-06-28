f0fae0d31b622293e74d04e12ae4263e2164dcb0
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-06-28 19:57:29 +0530

    modified build.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,7 @@
-#!/usr/bin/env bash
-# Exit on error
+set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+# Install dependencies
+python3 -m pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
+python3 manage.py collectstatic 
